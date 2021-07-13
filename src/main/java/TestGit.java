8fb0ad7a9e79ace5ec8938f519c53137ab567466
xujiahai <xujiahai0528@163.com>
2021-07-13 22:54:09 +0800

    第三个版本

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -2,5 +2,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("自己创建的git本地库");
         System.out.println("第二个版本");
+        System.out.println("第三个版本");
     }
 }
