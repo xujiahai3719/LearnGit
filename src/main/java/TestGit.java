0be2ebf354bc422d0df2557b5db34199b09ef2d8
xujiahai <xujiahai0528@163.com>
2021-07-13 23:47:35 +0800

    主分支也来一次新增的内容

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -7,5 +7,6 @@ public class TestGit {
         System.out.println("主分支添加的内容");
         System.out.println("分支增加的内容");
         System.out.println("分支最后一次新增的内容");
+        System.out.println("主分支也来一次新增内容");
     }
 }
